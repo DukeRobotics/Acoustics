eb842be972d16e96d5e30a1f7577334a41cc332a
ler24@duke.edu <ler24@duke.edu>
2017-07-27 01:57:50 -0700

    Ready for motion planner, refactored a bit

--- AcousticsCapture/main.py
+++ AcousticsCapture/main.py
@@ -6,8 +6,15 @@ import math
 import saleae
 import time
 import os
+import psutil
 
-def read_file_mat(filename, numHeaders=2):
+#This should theoretically be 7.09 to 7.1, but experimentally (Due to speed of sound in different water ) is changed to 7.5.
+MAX_CORR_CONST = 7.09
+RANGE_CONST = 2556
+PING_CAP_DIR = "/home/robot/pingCaptures/"
+
+
+def readFileMat(filename, numHeaders=2):
     reader = csv.reader(open(filename), delimiter=',')
     total = [[], [], [], []]
     for row in reader:
@@ -19,55 +26,49 @@ def read_file_mat(filename, numHeaders=2):
     channels = [col for col in total[1:len(total)]]
     return channels
 
-
-def movingAverage(values, window):
-    weights = np.repeat(1.0, window) / window
-    sma = np.convolve(values, weights, 'valid')
-    return sma
-
-
-def moving_average(arr, n):
+def movingAverage(arr, n):
     return np.append(np.array([np.mean(arr[:i + 1]) for i in range(n - 1)]),
                      np.correlate(arr, np.ones(n, dtype=float) / n))
 
 
-def detect_wavefront(signal):
-    # tune these two constants to make it more robust / reduce lag (increasing sn will reduce false positives, but increase lag)
+def detectWavefront(signal):
+    # tune these two constants to make it more robust / reduce lag (increasing sn will
+    # reduce false positives, but increase lag)
     n = 10000
     sn = 1000
     thresh = 3  # tune this to similarly adjust robustness
 
     amplitude = np.abs(signal)
 
-    longma = moving_average(amplitude, n)
-    shortma = moving_average(amplitude, sn)
+    longma = movingAverage(amplitude, n)
+    shortma = movingAverage(amplitude, sn)
     return np.argmax(shortma > thresh * longma)
 
 
 def findBearing(capturePath, pingerFrequency, fs, sampleTime):
-    # Matrix = read_file_mat('dat1-left45.csv')
-    Matrix = read_file_mat(capturePath)
+    # matrix = read_file_mat('dat1-left45.csv')
+    matrix = readFileMat(capturePath)
 
     [b, a] = signal.cheby2(3, 3, [((pingerFrequency - 8) / fs * 2), ((pingerFrequency + 8) / fs * 2)], btype='bandpass')
-    fil_ch1 = signal.lfilter(b, a, np.asarray(Matrix[0]), axis=0)
+    fil_ch1 = signal.lfilter(b, a, np.asarray(matrix[0]), axis=0)
     # plt.plot(fil_ch1)
     # plt.show()
 
-    start = detect_wavefront(fil_ch1[:, 0])
-    print("detected wavefront in channel 0 at index %d, %.2fs" % (start, (start/len(fil_ch1))*sampleTime))
+    start = detectWavefront(fil_ch1[:, 0])
+    print("detected wavefront in channel 0 at index %d, %.2fs" % (start, (start / len(fil_ch1)) * sampleTime))
 
-    ra = 2556 # range constant
-    Channel0 = np.array(np.asarray(Matrix[0]))[start - 1:start + ra - 1]  # 241925:242005  113920:114000
-    Channel1 = np.array(np.asarray(Matrix[1]))[start - 1:start + ra - 1]
-    Channel2 = np.array(np.asarray(Matrix[2]))[start - 1:start + ra - 1]
+    ra = RANGE_CONST  # range constant
+    channel0 = np.array(np.asarray(matrix[0]))[start - 1:start + ra - 1]  # 241925:242005  113920:114000
+    channel1 = np.array(np.asarray(matrix[1]))[start - 1:start + ra - 1]
+    channel2 = np.array(np.asarray(matrix[2]))[start - 1:start + ra - 1]
 
-    FiltChannel0 = signal.lfilter(b, a, Channel0)
-    FiltChannel1 = signal.lfilter(b, a, Channel1)
-    FiltChannel2 = signal.lfilter(b, a, Channel2)
+    filtChannel0 = signal.lfilter(b, a, channel0)
+    filtChannel1 = signal.lfilter(b, a, channel1)
+    filtChannel2 = signal.lfilter(b, a, channel2)
 
-    diff11 = corr(FiltChannel1, FiltChannel0, fs,
+    diff11 = corr(filtChannel1, filtChannel0, fs,
                   pingerFrequency)  ##if all bearings are opposite of expected, switch order of 2 and 1
-    diff12 = corr(FiltChannel2, FiltChannel0, fs,
+    diff12 = corr(filtChannel2, filtChannel0, fs,
                   pingerFrequency)  ##if all bearings are opposite of expected, switch order of 3 and 1
 
     bearing = np.arctan2(diff11, diff12) * 180 / np.pi
@@ -87,7 +88,7 @@ def corr(waveA, waveB, fs, pingerFrequency):
     maxCorr = corrArray.index(max(corrArray))  ##MAY NEED TO SUBTRACT ONE??
     # print(corrArray)
 
-    if maxCorr > 7.09:  ##This should theoretically be 7.09 to 7.1, but experimentally (Due to speed of sound in different water ) is changed to 7.5.
+    if maxCorr > MAX_CORR_CONST:
         maxCorr = maxCorr - maxLag
         # if maxCorr<-7.09: ##this should also be -7.09
     # raise("error: not possible")   may not need it
@@ -105,35 +106,43 @@ def getPingerData(host='localhost', port=10429):
     s.capture_start_and_wait_until_finished()
 
     t = time.time()
-    file_path_on_target_machine = str("/home/robot/pingCaptures/" + str(t) + ".csv")
+    file_path_on_target_machine = str(PING_CAP_DIR + str(t) + ".csv")
     s.export_data2(file_path_on_target_machine,
                    digital_channels=digital,
                    analog_channels=analog,
                    format="csv",
                    analog_format="voltage"
                    )
-    return (t, file_path_on_target_machine)
+    return t, file_path_on_target_machine
+
+
+def checkLogicRunning():
+    candidates = []
+    for proc in psutil.process_iter():
+        try:
+            if 'logic' in proc.name().lower():
+                candidates.append(proc)
+        except psutil.NoSuchProcess:
+            pass
+    if len(candidates) == 0:
+        return False
+    if len(candidates) >= 1:
+        return True
+
 
 def startLogic():
-    os.system("Logic &")
-    time.sleep(12)
+    # Don't start if already running
+    if not checkLogicRunning():
+        os.system("Logic &")
+        time.sleep(12)
+
 
 def mainFunction():
-    # TimeOfPingOffset = recordFirstListen()
-    # getPingerData(TimerClock=TimeOfPingOffset)
-    # findPhaseDifference(pingerFrequency, 625000)
-    # print('starting')
-    # print('found bearing: %d' % findBearing(35000, 625000, 2.2))
-
-    # ONLY NEEDS TO RUN ONCE
-    startLogic()
-
-    print('begin live capture and detect')
-    # THIS ONLY RUNS IF THE LOGIC PROCESS IS ALIVE!
-    while (True):
+    # REQUIRES X
+    while True:
+        startLogic()
         pdat, time = getPingerData()
         print('found bearing from time %d: %d degrees' % (str(time), findBearing(pdat, 35000, 625000, 2.2)))
-
 
 
 mainFunction()

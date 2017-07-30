b5d2d635ddd80d26f252a6019bd2e1665fdaa546
estellehe <estellehe729@gmail.com>
2017-07-30 13:03:26 -0700

    Maybe working version at the end of robosub2017
    This need to be integrated with saleae running part, works for the pool in SD but not transdec data, should be the same algorithm as the matlab file

--- Misc/hydro_calculation.py
+++ Misc/hydro_calculation.py
@@ -4,6 +4,7 @@ from scipy import signal
 #import saleae
 #import time
 import math
+#import matplotlib.pyplot as plt
 
 
 def read_file_mat(filename, numHeaders=2):
@@ -18,26 +19,61 @@ def read_file_mat(filename, numHeaders=2):
 	channels = [col for col in total[1:len(total)]]
 	return channels
 
+def movingAverage(arr, n):
+	return np.append(np.array([np.mean(arr[:i + 1]) for i in range(n - 1)]),
+                     np.correlate(arr, np.ones(n, dtype=float) / n))
 
-def findPhaseDifference(pingerFrequency=25000, fs=625000):
-    Matrix=read_file_mat('dat1-left45.csv')
-    start = 219000
-    ra = 2556
-    Channel0=np.array(np.asarray(Matrix[0]))[start-1:start+ra-1] #241925:242005  113920:114000
-    Channel1=np.array(np.asarray(Matrix[1]))[start-1:start+ra-1]
-    Channel2=np.array(np.asarray(Matrix[2]))[start-1:start+ra-1]
+def detectWavefront(signals, pingerFrequency, fs):
+    # tune these two constants to make it more robust / reduce lag (increasing sn will
+    # reduce false positives, but increase lag)
+    signals = filterWave(signals, pingerFrequency, fs)
+    #print(signals[0:10])
+    n = 10000
+    sn = 1000
+    thresh = 4 # tune this to similarly adjust robustness
+    amplitude = np.abs(signals)
+    longma = movingAverage(amplitude, n)
+    shortma = movingAverage(amplitude, sn)
+    return np.argmax(shortma > thresh * longma)
+
+def filterWave(signals, pingerFrequency=40000, fs=625000):
+	b, a = signal.cheby2(3, 3, [((pingerFrequency - 8) / fs * 2), ((pingerFrequency + 8) / fs * 2)], btype='bandpass')
+	filteredwave = signal.lfilter(b, a, signals, axis=0)
+	filteredwave = [x for n in filteredwave for x in n]
+	return filteredwave
+
+def findPhaseDifference(filename, add, pingerFrequency=40000, fs=625000):
+	Matrix=read_file_mat(filename)
+	ra = 2556
+
+
+	Channel0=np.array(np.asarray(Matrix[0]))
+	Channel1=np.array(np.asarray(Matrix[1]))
+	Channel2=np.array(np.asarray(Matrix[2]))
     #NEED TO SHORTEN WINDOW--DEPENDS ON USAGE
-    b, a=signal.cheby2(3, 3, [((pingerFrequency-8)/fs*2),((pingerFrequency+8)/fs*2)], btype='bandpass')
-    FiltChannel0=signal.lfilter(b,a,Channel0)
-    FiltChannel1=signal.lfilter(b,a,Channel1)
-    FiltChannel2=signal.lfilter(b,a,Channel2)
+	ping = detectWavefront(Channel0, pingerFrequency, fs)
+	end = len(Channel0)
 
-    diff11=corr(FiltChannel1, FiltChannel0, fs, pingerFrequency) ##if all bearings are opposite of expected, switch order of 2 and 1
-    diff12=corr(FiltChannel2, FiltChannel0, fs, pingerFrequency) ##if all bearings are opposite of expected, switch order of 3 and 1
+	Channel0s = Channel0[ping+add-1:ping+add+ra-1]
+	Channel1s = Channel1[ping + add - 1:ping+add+ra - 1]
+	Channel2s = Channel2[ping + add - 1:ping+add+ra - 1]
+	#b, a=signal.cheby2(3, 3, [((pingerFrequency-8)/fs*2),((pingerFrequency+8)/fs*2)], btype='bandpass')
+	#FiltChannel0=signal.lfilter(b,a,Channel0, axis=0)
+	#FiltChannel1=signal.lfilter(b,a,Channel1, axis=0)
+	#FiltChannel2=signal.lfilter(b,a,Channel2, axis=0)
+	FiltChannel0 = filterWave(Channel0s, pingerFrequency, fs)
+	FiltChannel1 = filterWave(Channel1s, pingerFrequency, fs)
+	FiltChannel2 = filterWave(Channel2s, pingerFrequency, fs)
 
-    bearing=np.arctan2(diff11, diff12)*180/np.pi
-    print ("bearing is: %.5f" % bearing)
-    return bearing
+	print(ping+add)
+
+	#print(fChannel0s[0:10])
+	diff11=corr(FiltChannel1, FiltChannel0, fs, pingerFrequency) ##if all bearings are opposite of expected, switch order of 2 and 1
+	diff12=corr(FiltChannel2, FiltChannel0, fs, pingerFrequency) ##if all bearings are opposite of expected, switch order of 3 and 1
+
+	bearing=np.arctan2(diff11, diff12)*180/np.pi
+	print ("bearing is: %.5f" % bearing)
+	return bearing
 
 def corr(waveA, waveB, fs, pingerFrequency):
 	maxLag=fs/pingerFrequency
@@ -47,9 +83,9 @@ def corr(waveA, waveB, fs, pingerFrequency):
 	#print(waveB)
 	for i in range(math.floor(maxLag-1)): #DO YOU NEED TO SUBTRACT ONE?
 		#print(np.corrcoef(waveA[:n,0], waveB[i:n+i,0]))
-		corrArray.append(np.corrcoef(waveA[:n,0], waveB[i:n+i,0])[0][1])
+		corrArray.append(np.corrcoef(waveA[:n], waveB[i:n+i])[0][1])
 	maxCorr=corrArray.index(max(corrArray)) ##MAY NEED TO SUBTRACT ONE??
-	print(corrArray)
+	#print(corrArray)
 
 	if maxCorr>7.09: ##This should theoretically be 7.09 to 7.1, but experimentally (Due to speed of sound in different water ) is changed to 7.5.
 		maxCorr=maxCorr-maxLag
@@ -58,4 +94,21 @@ def corr(waveA, waveB, fs, pingerFrequency):
 	print (maxCorr)
 	return maxCorr
 
- print (findPhaseDifference(35000, 625000))
+
+def findPing(wave, fs = 625000):
+	window = fs/625000*2000
+	maxPing = max(wave)
+	maxID = wave.tolist().index(maxPing)
+	result = maxID - window/2
+	return result, maxPing
+
+
+def bearing(filename, pingerFrequency = 25000, fs = 625000):
+	bearinglist = []
+	for i in range(3):
+		i = i*100
+		bearinglist.append(findPhaseDifference(filename, i, pingerFrequency, fs))
+	return (sum(bearinglist)/len(bearinglist))
+
+
+print(bearing('july271019.csv', 30000, 625000))

e53a25c5af94e2e0ae87bae446e89c60dbb3945c
Estelle He <wh103@duke.edu>
2018-01-27 15:20:42 -0500

    detect max freq

--- test.c
+++ test.c
@@ -9,25 +9,37 @@ int main(int argc, char *argv[]) {
   fftw_complex *out;
   double *in;
   fftw_plan p;
-  int N = 100;
   int i;
+  int freqs = 12000;
+  int freq = 6000;
+  int times = 4;
+  int count = times*freqs;
+  int freqmin = 2000;
+  int freqmax = 6000;
+  int result = 0;
+  int f;
 
 
-  in = (double*) fftw_malloc(sizeof(double) * N);
-  out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
-  p = fftw_plan_dft_r2c_1d(N, in, out, FFTW_ESTIMATE);
+  in = (double*) fftw_malloc(sizeof(double) * count);
+  out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * count);
+  p = fftw_plan_dft_r2c_1d(count, in, out, FFTW_ESTIMATE);
 
 
-  for (i = 0; i < N; i++) {
-    in[i] = cos(N/(4*3.14)*i);
+  for (i = 0; i < count; i++) {
+    in[i] = cos(2*3.1415926*i*freq/freqs);//2pi*freq*time, time = i/freqs, 2pi*freq/freqs*i
   }
 
   fftw_execute(p); /* repeat as needed */
   fftw_destroy_plan(p);
 
-  for (i = 0; i < (N/2+1); i++) {
-    printf("%f %f\n", out[i][0], out[i][1]);
+  for (i = (freqmin*times); i <= (freqmax*times); i++) {
+    f = i/times;
+    if (f > result) {
+      result = f;
+    }
+    printf("%d %f %f\n", f, out[i][0], out[i][1]);
   }
+  printf("max is %d Hz\n", f);
 
   fftw_free(in); fftw_free(out);
 }

a7d2e4a9104563efbf5dc60b3bed02e422e5737b
estellehe <estelle.he@duke.edu>
2019-03-02 10:21:08 -0800

    saleae tested

--- saleae_sampling.py
+++ saleae_sampling.py
@@ -2,13 +2,18 @@ import saleae
 import subprocess
 import time
 
+# 2 = 1250 kS/s, 3 = 625 kS/s, 4 = 125 kS/s
+sampling_rate = 3
+
 if __name__ == "__main__":
 
     try:
         s = saleae.Saleae()
     except:
-        subprocess.call(["/home/estellehe/Desktop/Logic/Logic"])
+        subprocess.Popen(["/home/estellehe/Desktop/Logic/Logic"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        time.sleep(15)
         s = saleae.Saleae()
+        print("saleae software down, open saleae software before next script run")
         #print("software not opened or no device detected")
         #exit()
 
@@ -16,12 +21,10 @@ if __name__ == "__main__":
     try:
         if s.get_active_device().active:
             s.set_active_channels([], [0, 1, 2, 3])
-            s.set_capture_seconds(3)
-            s.set_sample_rate(s.get_all_sample_rates()[0])
-            print(s.get_all_sample_rates()[0])
+            s.set_capture_seconds(1)
+            s.set_sample_rate(s.get_all_sample_rates()[sampling_rate])
     except:
         exit()
-
 
     s.capture_start_and_wait_until_finished()
 
@@ -33,6 +36,4 @@ if __name__ == "__main__":
 
     #todo: add a while loop to wait till process is finished and then export data
     if s.is_processing_complete():
-        print time.time()
         s.export_data2("/home/estellehe/Desktop/Acoustics/saleae_data.csv", analog_channels=[0, 1, 2, 3])
-        print time.time()

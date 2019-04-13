9c8fc54e2fec50b1128098e793fa7e1dba02bc2e
estellehe <estelle.he@duke.edu>
2019-04-13 09:13:56 -0700

    another fix

--- saleae_sampling.py
+++ saleae_sampling.py
@@ -9,7 +9,7 @@ sampling_rate = 3
 fs = 625000
 pingc = pingc = fs*0.004
 
-output_path = "/home/robot/Documents/output/"
+output_path = "/home/estellehe/Desktop/output/"
 
 def moving_average_max(a, n = pingc) :
     weights = np.repeat(1.0, n)/n
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     try:
         s = saleae.Saleae()
     except:
-        subprocess.Popen(["/home/robot/Logic/Logic"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.Popen(["/home/estellehe/Desktop/Logic/Logic"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         time.sleep(15)
         s = saleae.Saleae()
         print("saleae software down, open saleae software before next script run")
@@ -48,16 +48,16 @@ if __name__ == "__main__":
     # except IOError:
     #     file = open(sys.argv[1], 'w')
     fn = False
-    fn = input("filename: x y z version: ")
+    fn = input("filename: x y z version: ").split(' ')
     while(fn):
+        file_name = "625k_40k_"+fn[0]+"_"+fn[1]+"_"+fn[2]+"("+fn[3]+").csv"
         fn = False
         s.capture_start_and_wait_until_finished()
-        file_name = "625k_40k_"+fn[0]+"_"+fn[1]+"_"+fn[2]+"("+fn[3]+").csv"
-        s.export_data2(output_path+"output1/", analog_channels=[0, 1, 2, 3])
+        s.export_data2(output_path+file_name, analog_channels=[0, 1, 2, 3])
 
         time.sleep(10)
         print("finish sampling")
-        fn = input("filename: x y z version")
+        fn = input("filename: x y z version: ").split(' ')
 
     # s.capture_start_and_wait_until_finished()
     # s.export_data2(output_path+"output2/"+sys.argv[1], analog_channels=[0, 1, 2, 3])

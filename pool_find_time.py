4b3ef60feea47e2d1eec2cb6e1a80b393fc65ce4
Estelle He <wh103@duke.edu>
2019-04-23 18:49:02 -0400

    untested timing script

--- pool_find_time.py
+++ pool_find_time.py
@@ -21,6 +21,7 @@ output_dir = "/home/robot/Documents/output/"
 filter_output_dir = "/home/robot/Documents/output/filtered/"
 
 #get bandpass filter parameter
+#bandwidth need to be 400
 def cheby2_bandpass(lowcut, highcut, fs, order=3):
     nyq = 0.5 * fs
     low = lowcut / nyq
@@ -76,7 +77,11 @@ if __name__ == "__main__":
 
     ts = False
     ts = input("sample length, sample delay, bandpass: ").split(' ')
-    while(ts):
+    while ts:
+        s_len = float(ts[0])
+        s_delay = float(ts[1])
+        bw = float(ts[2])
+
         # initial 3 second sampling
         s.set_capture_seconds(3)
         t_3s = time.time()
@@ -85,7 +90,7 @@ if __name__ == "__main__":
         t_3s_f = time.time()
         print("3 second starts at "+str(t_3s)+" and finishs at "+str(t_3s_f))
 
-        time.sleep(6)
+        time.sleep(3)
         # import data from csv
         df = pandas.read_csv(os.path.join(output_dir, "3s_"+str(t_3s).replace('.', '_')+".csv"), skiprows=[1], skipinitialspace=True)
         data1 = df["Channel 0"].tolist()
@@ -96,10 +101,10 @@ if __name__ == "__main__":
 
         #bandpass
         try:
-            out1 = cheby2_bandpass_filter(data1, freq-int(ts[2])/2, freq+int(ts[2])/2, fs)
-            out2 = cheby2_bandpass_filter(data2, freq-int(ts[2])/2, freq+int(ts[2])/2, fs)
-            out3 = cheby2_bandpass_filter(data3, freq-int(ts[2])/2, freq+int(ts[2])/2, fs)
-            out4 = cheby2_bandpass_filter(data4, freq-int(ts[2])/2, freq+int(ts[2])/2, fs)
+            out1 = cheby2_bandpass_filter(data1, freq-bw/2, freq+bw/2, fs)
+            out2 = cheby2_bandpass_filter(data2, freq-bw/2, freq+bw/2, fs)
+            out3 = cheby2_bandpass_filter(data3, freq-bw/2, freq+bw/2, fs)
+            out4 = cheby2_bandpass_filter(data4, freq-bw/2, freq+bw/2, fs)
         except Exception as e:
             print(e)
 
@@ -122,65 +127,83 @@ if __name__ == "__main__":
         avem3 = moving_average_max(outsq3)+150
         avem4 = moving_average_max(outsq4)+150
         first_front = min(avem1, avem2, avem3, avem4)/fs + t_3s
-        max_3s = max(max(outsq1), max(outsq2), max(outsq3), max(outsq4))
+        max_3s = max(outsq1+outsq2+outsq3+outsq4)
 
-        # do 0.09 second sampling, 0.02 second before the first first_front
-        # get next cycle start time
-        cycle = 0
-        while cycle*t_cycle+first_front-float(ts[1]) < time.time():
-            cycle = cycle + 1
-        next_start = cycle*t_cycle+first_front-float(ts[1])
-        latest_front = cycle*t_cycle+first_front
-        # wait for start time and do 0.5 second sampling
-        while time.time() < next_start:
-            pass
-        s.set_capture_seconds(float(ts[0]))
-        t_1s_1 = time.time()
-        s.capture_start_and_wait_until_finished()
-        s.export_data2(os.path.join(output_dir, "1s_"+str(t_1s_1).replace('.', '_')+".csv"), analog_channels=[0, 1, 2, 3])
-        t_1s_1_f = time.time()
-        print("1 second starts at "+str(t_1s_1)+" and finishs at "+str(t_1s_1_f))
+        r_mag = 0
+        r_index = 0
+
+        # every adjustment of sampling period move the offset 0.01s forward
+        # want the max stay in the second fourth of sampling duration(0.08s)
+        while r_mag < 2.0/3.0 or r_index < 1.0/4.0 or r_index > 1.0/2.0:
+            print("1s-to-3s magnitude ratio is "+str(r_mag))
+            print("max index ratio is "+str(r_index))
+
+            if r_mag != 0:
+                if r_mag < 2.0/3.0:
+                    s_delay = s_delay + 0.01
+                else:
+                    if r_index < 1.0/4.0:
+                        s_delay = s_delay + 0.01
+                    elif r_index > 1.0/2.0:
+                        s_delay = s_delay - 0.01
+            # do 0.08 second sampling, 0.02 second before the first first_front, so offset = -0.02
+            # get next cycle start time
+            cycle = 0
+            while cycle*t_cycle+first_front-s_delay < time.time():
+                cycle = cycle + 1
+            next_start = cycle*t_cycle+first_front-s_delay
+            latest_front = cycle*t_cycle+first_front
+            # wait for start time and do 0.5 second sampling
+            while time.time() < next_start:
+                pass
+            s.set_capture_seconds(s_len)
+            t_1s_1 = time.time()
+            s.capture_start_and_wait_until_finished()
+            s.export_data2(os.path.join(output_dir, "1s_"+str(t_1s_1).replace('.', '_')+".csv"), analog_channels=[0, 1, 2, 3])
+            t_1s_1_f = time.time()
+            print("1 second starts at "+str(t_1s_1)+" and finishs at "+str(t_1s_1_f))
 
 
-        time.sleep(2)
-        # import data from csv
-        df = pandas.read_csv(os.path.join(output_dir, "1s_"+str(t_1s_1).replace('.', '_')+".csv"), skiprows=[1], skipinitialspace=True)
-        data1 = df["Channel 0"].tolist()
-        data2 = df["Channel 1"].tolist()
-        data3 = df["Channel 2"].tolist()
-        data4 = df["Channel 3"].tolist()
-        print("import length "+str(len(data1)))
+            time.sleep(1)
+            # import data from csv
+            df = pandas.read_csv(os.path.join(output_dir, "1s_"+str(t_1s_1).replace('.', '_')+".csv"), skiprows=[1], skipinitialspace=True)
+            data1 = df["Channel 0"].tolist()
+            data2 = df["Channel 1"].tolist()
+            data3 = df["Channel 2"].tolist()
+            data4 = df["Channel 3"].tolist()
+            print("import length "+str(len(data1)))
 
-        #bandpass
-        try:
-            out1 = cheby2_bandpass_filter(data1, freq-int(ts[2])/2, freq+int(ts[2])/2, fs)
-            out2 = cheby2_bandpass_filter(data2, freq-int(ts[2])/2, freq+int(ts[2])/2, fs)
-            out3 = cheby2_bandpass_filter(data3, freq-int(ts[2])/2, freq+int(ts[2])/2, fs)
-            out4 = cheby2_bandpass_filter(data4, freq-int(ts[2])/2, freq+int(ts[2])/2, fs)
-        except Exception as e:
-            print(e)
+            #bandpass
+            try:
+                out1 = cheby2_bandpass_filter(data1, freq-bw/2, freq+bw/2, fs)
+                out2 = cheby2_bandpass_filter(data2, freq-bw/2, freq+bw/2, fs)
+                out3 = cheby2_bandpass_filter(data3, freq-bw/2, freq+bw/2, fs)
+                out4 = cheby2_bandpass_filter(data4, freq-bw/2, freq+bw/2, fs)
+            except Exception as e:
+                print(e)
 
-        print("filtered length "+str(len(out1)))
+            print("filtered length "+str(len(out1)))
 
-        df = pandas.DataFrame()
-        df["out1"] = out1;
-        df["out2"] = out2;
-        df["out3"] = out3;
-        df["out4"] = out4;
-        df.to_csv(os.path.join(filter_output_dir, "1s_"+str(t_1s_1).replace('.', '_')+"_filtered.csv"))
+            df = pandas.DataFrame()
+            df["out1"] = out1;
+            df["out2"] = out2;
+            df["out3"] = out3;
+            df["out4"] = out4;
+            df.to_csv(os.path.join(filter_output_dir, "1s_"+str(t_1s_1).replace('.', '_')+"_filtered.csv"))
 
-        #find first front with moving_average_max
-        outsq1 = np.absolute(out1[150:])
-        outsq2 = np.absolute(out2[150:])
-        outsq3 = np.absolute(out3[150:])
-        outsq4 = np.absolute(out4[150:])
-        max_1s = max(max(outsq1), max(outsq2), max(outsq3), max(outsq4))
-        maxi1 = np.argmax(outsq1)+150
-        maxi2 = np.argmax(outsq2)+150
-        maxi3 = np.argmax(outsq3)+150
-        maxi4 = np.argmax(outsq4)+150
-        index = np.sum(avem1, avem2, avem3, avem4)/4
-        print("max 3s is "+str(max_3s)+" and max 1s is "+str(max_1s)+" at index "+str(index))
+            #find first front with moving_average_max
+            outsq1 = np.absolute(out1[150:])
+            outsq2 = np.absolute(out2[150:])
+            outsq3 = np.absolute(out3[150:])
+            outsq4 = np.absolute(out4[150:])
+            total = outsq1+outsq2+outsq3+outsq4
+            max_1s = max(total)
+            index = np.argmax(total)+150
+            print("max 3s is "+str(max_3s)+" and max 1s is "+str(max_1s)+" at index "+str(index))
+
+            r_mag = max_1s/max_3s
+            r_index = index/(s_len*fs)
+
 
         ts = False
         ts = input("sample length, sample delay, bandpass: ").split(' ')

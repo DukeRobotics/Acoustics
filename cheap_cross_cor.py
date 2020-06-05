55945c5a10234d6fefe78df1cfc905251009c697
Reed Chen <reed.chen@duke.edu>
2020-06-04 22:27:41 -0400

    added bandpass filter to correlation

--- cheap_cross_cor.py
+++ cheap_cross_cor.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-from scipy.signal import correlate
 import matplotlib.pyplot as plt
+from scipy.signal import butter, lfilter, freqz, correlate
 
 '''
 pinger location
@@ -19,6 +19,10 @@ hp4 = [0, 0, space];
 
 filepath = '../Data/matlab_custom_cheap_hydrophone_(1).csv'
 
+fs = 625000
+pinger_frequency = 40000
+band_width = 500
+
 octant = [0, 0, 0]
 
 
@@ -29,19 +33,58 @@ def main():
     c3 = df["Channel 2"].tolist()
     c4 = df["Channel 3"].tolist()
 
-    cross12 = correlate(c1, c2, mode='full') # 2560060 - 2560000 = 60. c1 is 60 samples later than c2
-    cross13 = correlate(c1, c3, mode='full')
-    cross14 = correlate(c1, c4, mode='full')
+    lowcut = pinger_frequency-band_width//2
+    highcut = pinger_frequency+band_width//2
 
-    x = np.argmax(np.array(cross12)) - len(c1) # positive
-    y = np.argmax(np.array(cross13)) - len(c1) # negative
-    z = np.argmax(np.array(cross14)) - len(c1) # negative
+    #plot_filter(lowcut, highcut, fs, 4)
+
+    f1 = butter_bandpass_filter(c1, lowcut, highcut, fs, order=4)
+    f2 = butter_bandpass_filter(c2, lowcut, highcut, fs, order=4)
+    f3 = butter_bandpass_filter(c3, lowcut, highcut, fs, order=4)
+    f4 = butter_bandpass_filter(c4, lowcut, highcut, fs, order=4)
+
+    print(len(f1))
+    plot([c1, f1])
+
+    cross12 = correlate(f1, f2, mode='full') # 2560060 - 2560000 = 60. c1 is about 60 samples later than c2
+    cross13 = correlate(f1, f3, mode='full')
+    cross14 = correlate(f1, f4, mode='full')
+
+    x = np.argmax(np.array(cross12)) - len(f1) # positive
+    y = np.argmax(np.array(cross13)) - len(f1) # negative
+    z = np.argmax(np.array(cross14)) - len(f1) # negative
 
     print(x, y, z)
 
     octant[0] = 1 if x > 0 else -1
     octant[1] = 1 if y > 0 else -1
     octant[2] = 1 if z > 0 else -1
+
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+
+def plot_filter(lowcut, highcut, fs, order):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    w, h = freqz(b, a, worN=2000)
+    plt.plot((fs * 0.5 / np.pi) * w, abs(h), label="order = %d" % order)
+    plt.plot([0, 0.5 * fs], [np.sqrt(0.5), np.sqrt(0.5)], '--', label='sqrt(0.5)')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Gain')
+    plt.grid(True)
+    plt.legend(loc='best')
+    plt.show()
 
 
 def plot(data):
